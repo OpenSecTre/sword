7fe0acaf665525aa0ff520aae8c9a0ccc7e545bf
Mika Turunen <mika.turunen@ymail.com>
2016-06-18 20:59:09 +0300

    #2 connection to mongo now works as it should - nice\!

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ require('./config/passport');
 
 var app = express();
 
-
+console.log("env for mongo:", process.env.MONGODB);
 mongoose.connect(process.env.MONGODB);
 mongoose.connection.on('error', function() {
   console.log('MongoDB Connection Error. Please make sure that MongoDB is running.');
