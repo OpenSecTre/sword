67b76d8797cbda87b39fc636c54aaa9cf479a743
Ville Seppälä <ville@villeseppala.com>
2016-06-19 21:47:14 +0300

    Add client layout #6

--- client/src/components/appmenu/AppMenu.js
+++ client/src/components/appmenu/AppMenu.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react'
 import Menu from 'material-ui/Menu'
 import MenuItem from 'material-ui/MenuItem'
 
+import IconSettings from 'material-ui/svg-icons/action/settings'
+import IconScan from 'material-ui/svg-icons/action/perm-scan-wifi'
+
 export class AppMenu extends Component {
   render () {
     return (
-      <Menu>
-        <MenuItem primaryText="Refresh" />
-        <MenuItem primaryText="Help &amp; feedback" />
-        <MenuItem primaryText="Settings" />
-        <MenuItem primaryText="Sign out" />
+      <Menu autoWidth={false}>
+        <MenuItem primaryText='Newtwork Scanning' leftIcon={<IconScan />} />
+        <MenuItem primaryText='Settings' disabled={true} leftIcon={<IconSettings />} />
       </Menu>
     )
   }
--- client/src/components/titlebar/TitleBar.js
+++ client/src/components/titlebar/TitleBar.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
 import AppBar from 'material-ui/AppBar'
+import classes from './_style.scss';
 
 export class TitleBar extends Component {
   render () {
     return (
-      <AppBar title="Title" />
+      <div className={classes.appbar}>
+        <h1 className={classes.title}>
+          SW<span className={classes.highlight}>o</span>RD
+        </h1>
+      </div>
     )
   }
 }
