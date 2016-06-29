819937a3b6992adcdd25ef1ec3a9b03787a0153b
Ville Seppälä <ville@villeseppala.com>
2016-06-29 19:37:51 +0300

    Add first implementation of Da Scanner #5

--- server/api/index.js
+++ server/api/index.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import facets from './facets';
+import PythonShell from 'python-shell';
 
 export default function() {
 	var api = Router();
@@ -11,6 +12,25 @@ export default function() {
 	api.get('/', (req, res) => {
 		res.json({
 			version : '1.0'
+		});
+	});
+
+	api.get('/scan', (req, res) => {
+		var options = {
+		  mode: 'text',
+		  pythonOptions: ['-u'],
+		  scriptPath: '../backend/',
+		  args: ['-t', '127.0.0.1', '-p', '1024']
+		};
+
+		PythonShell.run('net_scan.py', options, function (err, results) {
+		  if (err) throw err;
+		  // results is an array consisting of messages collected during execution
+		  console.log('results: %j', results);
+
+			res.json({
+				result: results
+			});
 		});
 	});
 
