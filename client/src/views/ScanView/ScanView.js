3baac1ca51fd43559fc18c57a4213cd2deeb1d26
Ville Seppälä <ville@villeseppala.com>
2016-06-29 17:23:47 +0300

    Fix linting

--- client/src/views/ScanView/ScanView.js
+++ client/src/views/ScanView/ScanView.js
@@ -2,18 +2,20 @@ import React, { Component } from 'react';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 
+const primaryButton = true;
+
 export class ScanView extends Component {
   render () {
     return (
       <div>
         <div>
           <TextField
-            hintText="192.168.1.0/24"
-            floatingLabelText="IP-address"
+            hintText='192.168.1.0/24'
+            floatingLabelText='IP-address'
           />
         </div>
         <div>
-          <RaisedButton label="Scan" primary={true} />
+          <RaisedButton label='Scan' primary={primaryButton} />
         </div>
         <div>
           <h2>Result:</h2>
