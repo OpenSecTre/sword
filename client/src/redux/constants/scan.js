07563eb89beb1d4fd4359e13ab0e65cece9db8de
Ville Seppälä <ville@villeseppala.com>
2016-07-07 00:19:40 +0300

    Add client server connection (scanning) #14

---
+++ client/src/redux/constants/scan.js
@@ -0,0 +1,6 @@
+export const SCAN_SUCCESS = 'SCAN_SUCCESS';
+export const SCAN_ERROR = 'SCAN_ERROR';
+export const SCAN_LOADING = 'SCAN_LOADING';
+
+export const CHANGE_IP = 'CHANGE_IP';
+export const CHANGE_PORT = 'CHANGE_PORT';
--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import { routerReducer as router } from 'react-router-redux';
+import scan from './scan';
 
 export default combineReducers({
-  router
+  router,
+  scan
 });
