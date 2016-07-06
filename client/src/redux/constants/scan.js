4fbee951ca3c63a9086ef641e2a80f7e4c6b331f
Ville Seppälä <ville@villeseppala.com>
2016-07-07 00:40:51 +0300

    Change redux to module structure #14

--- client/src/redux/constants/scan.js
+++
@@ -1,6 +0,0 @@
-export const SCAN_SUCCESS = 'SCAN_SUCCESS';
-export const SCAN_ERROR = 'SCAN_ERROR';
-export const SCAN_LOADING = 'SCAN_LOADING';
-
-export const CHANGE_IP = 'CHANGE_IP';
-export const CHANGE_PORT = 'CHANGE_PORT';
